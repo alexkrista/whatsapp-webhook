7651579660a0b5010fbfc2c966d908873097b6c8
alexkrista <farben.krista@gmail.com>
2026-07-18 16:44:05 +0200

    Fix: Zustandsmaschine, Tagesabschluss-Dialog (3 Phasen), doppelte-Klammer-Bug behoben

--- kristine.js
+++ kristine.js
@@ -1864,6 +1864,5 @@ function registerKristine(app, { dataDir, requireAdmin, publicDir, sendWhatsApp,
   // Derselbe Dialogkern wird vom Browser-Simulator und vom echten WhatsApp-Webhook verwendet.
   return { handleMessage, localDateISO };
 }
-}
 
 module.exports = { registerKristine };
