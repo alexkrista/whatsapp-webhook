607a1dd956886dd89b844592fb30f7eaa4f995b7
alexkrista <farben.krista@gmail.com>
2026-08-22 16:22:55 +0200

    Load invoice edit workflow before finance runtime

--- brain_incoming_op.py
+++ brain_incoming_op.py
@@ -1,8 +1,14 @@
 # coding: utf-8
-"""Kompatibilitäts-Loader für KRISTINE Finance V4.
+"""Kompatibilitäts-Loader für KRISTINE Finance V4 + Eingangsrechnungs-Bearbeitung.
 
 Die frühere OP-Erfassungsansicht wird durch den getrennten Bezahl-OP und den
-Revolut-Bereich ersetzt. brain_viewer_hotfix.py kann diesen Einstieg weiterhin
-unverändert importieren.
+Revolut-Bereich ersetzt. Zusätzlich werden erfasste, noch ungeprüfte Rechnungen
+wieder bearbeitbar gemacht.
 """
-from brain_finance_runtime import install
+from brain_finance_runtime import install as _finance_install
+from brain_capture_edit import install as _capture_edit_install
+
+
+def install(ns):
+    _capture_edit_install(ns)
+    _finance_install(ns)
