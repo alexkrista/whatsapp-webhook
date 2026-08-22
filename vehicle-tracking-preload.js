309b3b629cfd9d0ae1496af147fd385a4a125e0a
alexkrista <farben.krista@gmail.com>
2026-08-22 08:29:52 +0200

    Fahrzeugtracking: Bewegungsereignisse nicht als Zündung behandeln

--- vehicle-tracking-preload.js
+++ vehicle-tracking-preload.js
@@ -9,6 +9,21 @@ const { registerVehicleTracking } = require("./vehicle-tracking");
 
 function wrappedExpress(...args) {
   const app = originalExpress(...args);
+
+  // Traccar erzeugt neben echten Zündungsereignissen auch Moving/Stopped.
+  // Die dürfen eine Fahrt NICHT starten/beenden (Ampel, Stau, kurze Standzeit).
+  app.post(
+    "/kristine/api/vehicle-tracking/traccar/event",
+    originalExpress.json({ limit: "1mb" }),
+    (req, res, next) => {
+      const type = String(req.body?.type || "");
+      if (["deviceMoving", "deviceStopped"].includes(type)) {
+        return res.json({ ok: true, ignored: true, eventType: type, reason: "motion_event_not_ignition" });
+      }
+      return next();
+    }
+  );
+
   registerVehicleTracking(app, {
     express: originalExpress,
     dataDir: process.env.DATA_DIR || "/var/data",
