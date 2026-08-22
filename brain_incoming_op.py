7f4e0afe7fbeb105a18fa79970b488955beb1737
alexkrista <farben.krista@gmail.com>
2026-08-22 21:48:42 +0200

    Load enriched OP source and reporting tools

--- brain_incoming_op.py
+++ brain_incoming_op.py
@@ -13,7 +13,13 @@ import os
 # bewusst überschrieben werden, aber ohne Konfiguration geht die Aufgabe an Alex0780.
 os.environ.setdefault("KRISTINE_FINANCE_APPROVER_ID", "Alex0780")
 
-from brain_finance_runtime import install as _finance_install
+# Finance Runtime arbeitet ab hier mit der erweiterten Source V2. So bekommen die
+# bestehenden Endpunkte Skonto-Metadaten, ohne die stabile Runtime-Datei umzubauen.
+import brain_finance_runtime as _finance_runtime
+from brain_finance_source_v2 import FinanceStore as _FinanceStoreV2
+_finance_runtime.FinanceStore = _FinanceStoreV2
+_finance_install = _finance_runtime.install
+
 from brain_capture_edit import install as _capture_edit_install
 from brain_currency_payment_v2 import install as _currency_payment_install
 from brain_finance_test_bridge import install as _finance_test_bridge_install
@@ -23,6 +29,7 @@ from brain_finance_reconciliation import install as _reconciliation_install
 from brain_finance_reconciliation_bridge import install as _reconciliation_bridge_install
 from brain_invoice_intake import install as _invoice_intake_install
 from brain_home_nav import install as _home_nav_install
+from brain_finance_op_tools import install as _op_tools_install
 from brain_finance_header import install as _finance_header_install
 
 
@@ -37,4 +44,5 @@ def install(ns):
     _reconciliation_bridge_install(ns)
     _invoice_intake_install(ns)
     _home_nav_install(ns)
+    _op_tools_install(ns)
     _finance_header_install(ns)
