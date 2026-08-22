10f7b8814683ac39f1f35b2bb46fa968ecca65ca
alexkrista <farben.krista@gmail.com>
2026-08-22 16:34:42 +0200

    Load currency and payment capture extension

--- brain_incoming_op.py
+++ brain_incoming_op.py
@@ -3,12 +3,15 @@
 
 Die frühere OP-Erfassungsansicht wird durch den getrennten Bezahl-OP und den
 Revolut-Bereich ersetzt. Zusätzlich werden erfasste, noch ungeprüfte Rechnungen
-wieder bearbeitbar gemacht.
+wieder bearbeitbar gemacht und Zahlungsweg/Fremdwährung direkt bei der Erfassung
+geführt.
 """
 from brain_finance_runtime import install as _finance_install
 from brain_capture_edit import install as _capture_edit_install
+from brain_currency_payment import install as _currency_payment_install
 
 
 def install(ns):
     _capture_edit_install(ns)
     _finance_install(ns)
+    _currency_payment_install(ns)
